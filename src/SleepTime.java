7022933bd6773b4647695100fd3946560ea1c1e2
1851030 <1851030@01-19643.pusd.dom>
2022-04-05 14:17:38 -0700

    invader bouncing off frame sides -movement going down

--- src/SleepTime.java
+++ src/SleepTime.java
@@ -3,13 +3,13 @@ public class SleepTime {
     public static void main(String[] args) throws Exception {
     	
     	while(UFO.stop = true) { //if game is playing
-    		int sleepTime = SleepTime.getSleepTime(200,  2500); //timer for asteroids to fall
+    		int sleepTime = SleepTime.getSleepTime(2500); //timer for asteroids to fall
     		System.out.println("sleep time\t" + sleepTime);  //check time intervals
     		Thread.sleep(sleepTime);
     	}
     }
     
-    public static int getSleepTime(int min, int max) {
+    public static int getSleepTime(int sec) {
     	return 1500;
     }
 }
